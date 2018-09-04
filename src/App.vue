5d1d1036c9e229cbeb7f871d7e738737f410f45a
Jessica Costa <jessica.costa@justdigital.com>
2018-09-04 18:02:40 -0300

    Revelar e esconder cartas ao clicar

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <div id="app">
     <!-- <List title="Memory Game"/> -->
     <div class="app">
-      <img class="logo" src="./assets/marvel-logo.png" />
       <h1>{{ title }}</h1>
       <ul class="list" v-if="cards.length">
-        <li v-for="(item, index) in cards" :key="index">
-          <img class="img-cards" :src= " item.thumbnail.path + '.' + item.thumbnail.extension " :alt= "item.name" />
+        <li class="flipper" v-bind:class='{ "click" : (isActive[index])}' v-for="(item, index) in cards" :key="index">
+          <img class="img front" src="./assets/marvel-logo.png" alt="play!" @click="changeImg(index)" />
+          <img class="img back" :src= " item.thumbnail.path + '.' + item.thumbnail.extension " :alt= "item.name" @click="changeImg(index)"/>
         </li>
       </ul>
     </div>
@@ -18,13 +18,14 @@
 import { get } from "@/services/marvel";
 
 export default {
-  name: "app",
   components: {
 
   },
   data: () => ({
     title: "Memory Game",
-    cards: []
+    cards: [],
+    counter: 0,
+    isActive: {}, 
   }),
   mounted() {
     get()
@@ -35,16 +36,15 @@ export default {
             return !character.thumbnail.path.endsWith('image_not_available')
           }
         )
-        // if (game.data && game.data.results.length > 0) {
-          this.cards = filteredGame  
-        // } else {
-          // "http://i.annihil.us/u/prod/marvel/i/mg/6/20/52602f21f29ec"
-          // http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available
-
-        // }        
+        this.cards = filteredGame         
       }).catch((err) => {
 
       });
+  },
+  methods: {
+    changeImg: function(i) {
+      this.$set(this.isActive, i, !this.isActive[i])
+    }
   }
 };
 </script>
@@ -60,21 +60,13 @@ export default {
   text-align: center;
 }
 
-.logo {
-  height: 100px;
-  width: 200px;
-}
-
 .list {
-  // color: #ef0000;
   display: grid;
   font-weight: bold;
   list-style-type: none;
-  // grid-auto-flow: column;
   grid-gap: 54px;
   grid-template-columns: repeat(6, 240px);
-  // grid-template-rows: repeat(4, 300px);
-  // width: 1500px;
+  perspective: 1000; 
 
   li {
     margin-bottom: 10px;
@@ -82,12 +74,32 @@ export default {
   }
 }
 
-.img-cards {
+.flipper {
+  position: relative;
+  transform-style: preserve-3d;   
+  transition: 0.5s ease-in;
+}
+
+.click {
+  transform: rotateY(180deg); 
+}
+
+.img {
+  backface-visibility: hidden;   
   border: 2px solid #5f0214;
   border-radius: 3px;
   cursor: pointer;
   height: 250px;
   margin-top: 8px;
   width: 200px;
+}
+
+.front {
+  z-index: 2;
+  position: absolute;
+}
+
+.back {
+  transform: rotateY(180deg); 
 }
 </style>
